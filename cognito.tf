7f133546aa7d3ff76ef37419b9b916bb630926ed
Rodrigo Brasil <rodrigobbrasil@gmail.com>
2020-03-30 21:50:42 -0300

    client auth flows and other settings

--- cognito.tf
+++ cognito.tf
@@ -18,6 +18,9 @@ resource "aws_cognito_user_pool_client" "cognito_user_pool_client" {
     user_pool_id = aws_cognito_user_pool.cognito_user_pool.id
 
     allowed_oauth_flows = ["client_credentials"]
+    explicit_auth_flows = ["ALLOW_REFRESH_TOKEN_AUTH"]
+    supported_identity_providers = ["COGNITO"]
+    prevent_user_existence_errors = "ENABLED"
     allowed_oauth_flows_user_pool_client = true
     allowed_oauth_scopes = aws_cognito_resource_server.cognito_resource_server.scope_identifiers
     generate_secret = true
